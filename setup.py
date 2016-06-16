dbbe4e10615208bacc842917a8ffcd9f56e552ec
Eric Salina <ecsalina@gmail.com>
2016-06-15 21:56:16 -0400

    fix lang errors

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
 		"Intended Audience :: Developers",
 		"Topic :: Utilities",
 		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python :: 2.7"
-		"Programming Language :: Python :: 3.4"
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3.4",
 		"Programming Language :: Python :: 3.5"
 	]
 )
